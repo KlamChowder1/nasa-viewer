0fe2b6748876587eaf38112823137dabcc6dccd6
Kevin-BioDF <kevin.k.f.lam1@gmail.com>
2021-09-20 18:00:38 -0700

    01 - add title, rover name, date to images

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    hello
-    {{ this.images }}
-    <img v-for="image in images" :src="image.img_src" :key="image.id" />
+    <!-- hello
+    {{ this.images }} -->
+    <v-row>
+      <v-col cols="6" v-for="image in images" :key="image.id">
+        <v-card>
+          <v-card-title>
+            {{ image.id }}_{{ image.camera.full_name }}
+          </v-card-title>
+          📷 Credits:
+          <a href="https://www.instagram.com/marscuriosity/">{{
+            image.rover.name
+          }}</a>
+          on {{ image.earth_date }}
+          <img :src="image.img_src" contain height="50%" width="100%" />
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -20,9 +34,12 @@ export default {
     // console.log(process.env.VUE_APP_NASA_API_KEY);
     axios
       .get(
-        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-9-19&api_key=${process.env.VUE_APP_NASA_API_KEY}`
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-9-18&api_key=${process.env.VUE_APP_NASA_API_KEY}`
       )
-      .then((response) => (this.images = response.data.photos));
+      .then((response) => {
+        console.log(response.data.photos);
+        this.images = response.data.photos;
+      });
   },
 };
 </script>
